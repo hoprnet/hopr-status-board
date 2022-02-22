ae5360fae7a831bf3feb05b9b133dd6f515cdd62
Jose Aguinaga <me@jjperezaguinaga.com>
2022-02-22 13:52:09 +0100

    chore(gh-pages): Ensure proper domain for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hopr-status-board/',
   plugins: [react()],
   build: {
     target: ['es2020']
